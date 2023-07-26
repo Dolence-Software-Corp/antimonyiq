3964009effd61402a3c9cffe3000e1f8317ba5da
AntimonyIQ <antimonyiq@gmail.com>
2023-07-26 09:14:28 +0100

    Update server.js

--- server.js
+++ server.js
@@ -56,7 +56,7 @@ const server = http.createServer((req, res) => {
 
 });
 
-const port = 3100;
+const port = 3000;
 server.listen(port, ip, () => {
     const message = `Server running on http://localhost:${port}`;
     journal(message, source);
